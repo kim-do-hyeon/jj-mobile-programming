618d303b992b913e1abcdec17df02a01aa1970aa
“kim-do-hyeon” <pental@kakao.com>
2023-03-13 17:00:13 +0900

    20230313 3-상속/추상클래스 실습

---
+++ week2/exam11.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class exam11 {
+    public static void main(String args[]){
+        Animal animal;
+
+        animal = new Tiger();
+        animal.move();
+
+        animal = new Eagle();
+        animal.move();
+    }
+}
