070166551d1860f38a3637e22536e09d089dcdf1
kim-do-hyeon <pental@kakao.com>
2023-04-17 16:48:14 +0900

    Add multiple linear layout practice

---
+++ week7/MultipleLinearLayout/app/src/main/java/com/pental/multiplelinearlayout/MainActivity.java
@@ -0,0 +1,20 @@
+package com.pental.multiplelinearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
